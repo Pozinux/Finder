66c49dfd89ff5a8a621a977f3ca902e9e3811775
Timothé Poznanski <tim.poznanski@gmail.com>
2020-08-14 18:12:56 +0200

    Update constantes.py

--- app/constantes.py
+++ app/constantes.py
@@ -2,7 +2,7 @@ import os
 import logging
 
 # A décommenter pour voir les log dans tous les fichiers du projet ou "contantes.py" a été importé.
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - In %(filename)s - Line %(lineno)s - %(message)s')
+#logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - In %(filename)s - Line %(lineno)s - %(message)s')
 
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_DIR = os.path.join(CUR_DIR, "config")
@@ -12,6 +12,7 @@ DATA_DIR = os.path.join(CUR_DIR, "data")
 EXPORTS_VMWARE_DIR = os.path.join(EXPORTS_DIR, "exports_vmware")
 EXPORTS_OPCA_DIR = os.path.join(EXPORTS_DIR, "exports_opca")
 EXPORTS_CMDB_DIR = os.path.join(EXPORTS_DIR, "exports_cmdb")
+EXPORTS_CMDB_ALL_DIR = os.path.join(EXPORTS_DIR, "exports_cmdb_all")
 DB_SQLITE_FILE = os.path.join(DATA_DIR, "sqlite_db_file.db")
 
 logging.debug(f"DB_SQLITE_FILE -> {DB_SQLITE_FILE}")
